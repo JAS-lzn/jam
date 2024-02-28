26bf39753ced1a46fe8519887621708c74e18bf7
JAS-lzn <Joshua.Shearer64@gmail.com>
2024-02-27 18:55:38 -0600

    reading files out of dir

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,41 @@ mod fetchers;
 mod parsers;
 mod tests;
 
+use std::fs;
+
 use fetchers::*;
+use git2::Repository;
 use parsers::*;
 
 // public API exposed to consumers of `jam`
 // TODO: Make the input args real
 pub fn jam_finder(arg: String) {
-	// TODO: give args to let people choose to just grab diffs,
-	git_data(FetchOptions{repo_url: Some(arg), diff: None});
-	// TODO: getDiffs(versionA, versionB)
-	find_methods("uwu".to_string());
+    // TODO: give args to let people choose to just grab diffs,
+    git_data(FetchOptions {
+        repo_url: Some(arg),
+        diff: None,
+    });
+    // TODO: getDiffs(versionA, versionB)
+    find_methods("uwu".to_string());
+}
+
+// public API to obtain a list of diffs for the main branch of a repo
+pub fn diff_finder(arg_path: String) -> Vec<String> {
+    // check for a repo in memory
+    let has_working_dir = fs::metadata("./working_dir").is_ok();
+
+    // if we don't have a repo cloned, warn the user and exit
+    if has_working_dir == false {
+        print!("There is no repo in memory! Clone a repo first")
+    }
+
+    let repo = Repository::open("./working_dir/".to_owned() + &arg_path).unwrap();
+
+    print!("{}", repo.is_bare() as i32);
+
+    let list_of_files = find_methods("working_dir".to_owned());
+
+    return list_of_files;
+
+    // if we *DO* have a repo in memory, continue
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,18 @@
-
+use std::ptr::null;
 
 #[test]
-fn can_get_repo(){
-	crate::jam_finder("https://github.com/prusa3d/PrusaSlicer".to_string())
+fn can_read_files() {
+ let files = crate::diff_finder("m".to_owned());
+ assert!(files.len() > 1);
+
+ println!("{}", files.join(" "))
 }
+
+#[test]
+fn can_get_repo() {
+    crate::jam_finder("https://github.com/jmlopez-rod/m".to_string());
+
+    // cleanup
+    let _ = std::fs::remove_dir_all("/working_dir/m");
+}
+
